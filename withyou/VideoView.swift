2a5d3bb51fee2ff923682b8e42257a2924efecad
AbdullahEesa1 <skl_7@hotmail.com>
2024-12-13 12:37:48 +0300

    Done

---
+++ withyou/VideoView.swift
@@ -0,0 +1,29 @@
+//
+//  VideoView.swift
+//  withyou
+//
+//  Created by Abdullah on 12/06/1446 AH.
+//
+
+import UIKit
+
+class VideoView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
